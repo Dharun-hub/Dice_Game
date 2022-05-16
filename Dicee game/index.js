0edc348b5e16b9d8f63f9d8ccca578b63b023318
Dharun R <63845401+Dharun-hub@users.noreply.github.com>
2022-05-16 20:26:16 +0530

    Add files via upload

---
+++ Dicee game/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage = "dice" + randomNumber1 + ".png";
+var  randomImageSource = "images/" + randomImage;
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomImage2 ;
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "🚩 Player 1 wins";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 wins 🚩";
+}
+else{
+  document.querySelector("h1").textContent = "Draw !";
+}
